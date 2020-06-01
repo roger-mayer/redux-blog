016d703352505cc3ba554ec2154fe07c8ab98ee9
Roger Mayer <rmayer1984@gmail.com>
2020-05-31 21:34:24 -0500

    refactored to es6

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,9 @@
 //axios api
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 //action creator
-export const fetchPosts =  () => {
-    return async function (dispatch, getState) {
+export const fetchPosts = () => async dispatch => {
         const response = await jsonPlaceholder.get('/posts');
 
         dispatch({ type: 'FETCH_POSTS', PAYLOAD: response })
     };
-};
 
