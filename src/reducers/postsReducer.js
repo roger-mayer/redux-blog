f54f068b5a7209110b23ce2aec17c51211d33173
Roger Mayer <rmayer1984@gmail.com>
2020-06-16 12:27:43 -0500

    added note on adding to array

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -2,5 +2,10 @@ export default (state, action) => {
   //return state + action
     //mutation means changing elements in an array/object
 
+
+    //creating/adding to an array:
+    //const colors = ['red', 'green']
+    //[...colors, 'blue]
+
 };
 
