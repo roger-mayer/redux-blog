8344ee9918f21c9679bc49444f74a0463cb18d37
Roger Mayer <rmayer1984@gmail.com>
2020-07-10 17:55:26 -0500

    starting post reducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -2,13 +2,16 @@ export default (state, action) => {
   //return state + action
     //mutation means changing elements in an array/object
 
-
     //creating/adding to an array:
     //const colors = ['red', 'green']
     //[...colors, 'blue']
 
     //removing element
     //colors.filter(color => color !== 'blue')
+    if (action.type === 'FETCH_POSTS') {
+        return action.payload;
+    }
+
 
 };
 
