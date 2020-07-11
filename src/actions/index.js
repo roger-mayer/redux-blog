0c103ccc736b6fe981003b3827dcb5efd88ea505
Roger Mayer <rmayer1984@gmail.com>
2020-07-10 19:06:10 -0500

    getting unhandled exception. tried returning state

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,6 @@ import jsonPlaceholder from "../apis/jsonPlaceholder";
 export const fetchPosts = () => async dispatch => {
         const response = await jsonPlaceholder.get('/posts');
 
-        dispatch({ type: 'FETCH_POSTS', PAYLOAD: response })
-    };
+        dispatch({ type: 'FETCH_POSTS', PAYLOAD: response.data });
+};
 
