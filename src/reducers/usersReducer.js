e4cc03aa7c1cec06d1c088513e90b5c6c83dcfae
Roger Mayer <rmayer1984@gmail.com>
2020-07-25 22:08:29 -0500

    added user prop in render method

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,5 +1,8 @@
 export default (state = [], action) => {
     switch (action.type) {
-        return
+        case 'FETCH_USER':
+            return [...state, action.payload];
+        default:
+            return state;
     }
 }
