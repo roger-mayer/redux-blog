d9f76468f30ae85feab3a6d5f5e5cf45598990aa
Roger Mayer <rmayer1984@gmail.com>
2020-08-01 23:19:03 -0500

    import lodash and refactor

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 //axios api
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
@@ -10,9 +11,19 @@ export const fetchPosts = () => async dispatch => {
 };
 
 
-export const fetchUser = (id) => async dispatch => {
-        const response = await jsonPlaceholder.get(`/users/${id}`);
-        dispatch({ type: 'FETCH_USER', payload: response.data });
+// export const fetchUser = (id) => async dispatch => {
+//         const response = await jsonPlaceholder.get(`/users/${id}`);
+//
+//         dispatch({ type: 'FETCH_USER', payload: response.data });
+//
+// };
 
+export const fetchUser = function(id) {
+
+        return async function (dispatch) {
+                const response = await jsonPlaceholder.get(`/users/${id}`);
+
+                dispatch({type: 'FETCH_USER', payload: response.data});
+        }
 };
 
