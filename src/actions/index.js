c634280a622a3b00722a49ca870c9d4efb2257f7
Roger Mayer <rogermayer@rogersgekdomair.attlocal.net>
2020-08-09 21:06:24 -0500

    refactoring to add petchuserandposts

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,11 +11,26 @@ export const fetchPosts = () => async dispatch => {
 
 // need to define function outside of action creator
 export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+        const response = await jsonPlaceholder.get(`/users/${id}`);
+
+        dispatch({ type: 'FETCH_USER', payload: response.data });
+});
+
+//memoized version
+// export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async(id, dispatch) => {
+//         const response = await jsonPlaceholder.get(`/users/${id}`);
+//
+//         dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
+
+export const fetchPostsandUsers = (id) => dispatch => _fetchUser(id, dispatch);
 const _fetchUser = _.memoize(async(id, dispatch) => {
         const response = await jsonPlaceholder.get(`/users/${id}`);
 
         dispatch({ type: 'FETCH_USER', payload: response.data });
-
 });
+
+
 
 
