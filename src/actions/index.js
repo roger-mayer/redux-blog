75dc0aea3016f6cad3df2f96d7fb64788a0e29a3
Roger Mayer <rogermayer@rogersgekdomair.attlocal.net>
2020-08-09 20:59:03 -0500

    trying to get simgle memoization

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,8 +12,8 @@ export const fetchPosts = () => async dispatch => {
 // need to define function outside of action creator
 export const fetchUser = (id) => dispatch => {
         _fetchUser(id, dispatch);
-
 };
+
 const _fetchUser = _.memoize(async(id, dispatch) => {
         const response = await jsonPlaceholder.get(`/users/${id}`);
 
